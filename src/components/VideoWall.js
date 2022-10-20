4ba7b3dd149a9da7a911e1e60c006e2fdfb19600
lennhy <lennhydesign@gmail.com>
2022-10-19 20:28:00 -0400

    Add new line of code for api. Add components.

---
+++ src/components/VideoWall.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+
+const apiKey = process.env.REACT_APP_API_KEY;
+console.log(apiKey)
+const VideoWall = (props) => {
+    
+    const youtubeAPICall = () => {
+
+    }
+
+    const audioParam = () =>{
+
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default VideoWall;
